4c7e3e05a214613bcf7da13e07e70e087c351dda
Ran Bi <biran@iastate.edu>
2016-02-12 00:57:42 +0000

    version 1.2.4

--- R/check.power.R
+++ R/check.power.R
@@ -1,97 +1,112 @@
-#' Average Power and True FDR Based on Voom and Limma Pipeline
+#' Average Power and True FDR Based on limma/voom RNAseq Analysis Pipeline
 #' 
-#' For the voom and limma pipeline method, when we control false discovery rate by using both Benjamini 
-#' and Hochberg (1995) method and q-value procedure (Storey et al., 2004), \code{check.power} calculates
-#' average power and true FDR for given sample size, user-specified proportions of non-differentially 
-#' expressed genes, number of iterations, FDR level to control,
-#' mean counts in control group, dispersion, and log fold change.
+#' For the limma/voom RNAseq analysis pipeline, when we control false discovery
+#' rate by using the Benjamini and Hochberg step-up procedure (1995) 
+#' and/or Storey and Tibshirani's q-value procedure (Storey et al, 2004),
+#' \code{check.power} calculates average power and true FDR for given sample 
+#' size, user-specified proportions of non-differentially expressed genes, 
+#' number of iterations, FDR level to control, mean counts in control group, 
+#' dispersion, and log fold change.
 #' 
 #' @import qvalue
-#' @param arg a list of global parameters to pass into the function, such as total number of genes, 
-#' proportion of non-differentially expressed genes and treatment groups. See Details for more information.
-#' @param k number of iterations.
+#' @importFrom stats model.matrix p.adjust
+#'
+#' @param nGenes total number of genes, the default value is \code{10000}.
+#' @param pi0 proportion of non-differentially expressed genes, 
+#'            the default value is \code{0.8}.
+#' @param m sample size per treatment group.
+#' @param mu a vector (or scalar) of mean counts in control group 
+#'           from which to simulate.
+#' @param disp a vector (or scalar) of dispersion parameter 
+#'             from which to simulate.
+#' @param logfc a vector (or scalar, or a function that takes an integer n 
+#'                        and generates a vector of length n)
+#'              of log fold change for differentially expressed (DE) genes.  
+#' @param up proportion of up-regulated genes among all DE genes, 
+#'           the default value is \code{0.5}.
+#' @param replace sample with or without replacement from given parameters. 
+#'                See Details for more information.
 #' @param fdr the false discovery rate to be controlled.
-#' @param mu a scalar of mean counts in control group from which to simulate.
-#' @param disp a scalar of dispersion parameter from which to simulate.
-#' @param logfc log fold change between treatment group and control group.
+#' @param sims number of simulations to run when computing power and FDR.
 #' 
-#' @details \code{arg = list(nG, pi0, group)} where \code{nG} is the total number of genes, \code{pi0} 
-#' is the proportion of non-differentially expressed genes, and \code{group} is the treatment groups.
+#' @return \item{pow_bh_ave}{average power when controlling FDR 
+#'                           by Benjamini and Hochberg (1995) method.}
+#' @return \item{fdr_bh_ave}{true false discovery rate when controlling FDR 
+#'                           by Benjamini and Hochberg (1995) method.}
+#' @return \item{pow_bh_ave}{average power when controlling FDR 
+#'                           by q-value procedure (Storey et al., 2004).}
+#' @return \item{fdr_bh_ave}{true false discovery rate when controlling FDR
+#'                           by q-value procedure (Storey et al., 2004).}
 #' 
-#' @return \item{pow_bh_ave}{average power when controlling FDR by Benjamini 
-#' and Hochberg (1995) method.}
-#' @return \item{fdr_bh_ave}{true false discovery rate when controlling FDR by Benjamini 
-#' and Hochberg (1995) method.}
-#' @return \item{pow_bh_ave}{average power when controlling FDR by q-value procedure 
-#' (Storey et al., 2004).}
-#' @return \item{fdr_bh_ave}{true false discovery rate when controlling FDR by q-value procedure 
-#' (Storey et al., 2004).}
+#' @author Ran Bi \email{biran@@iastate.edu}, 
+#'         Peng Liu \email{pliu@@iastate.edu}
 #' 
-#' @author Ran Bi \email{biran@@iastate.edu}, Peng Liu \email{pliu@@iastate.edu}
+#' @references Benjamini, Y. and Hochberg, Y. (1995) 
+#'             Controlling the false discovery rate: a practical and 
+#'             powerful approach to multiple testing. 
+#'             \emph{J. R. Stat. Soc. B}, 57, 289-300.
 #' 
-#' @references Benjamini, Y. and Hochberg, Y. (1995) Controlling the false discovery rate: a practical and 
-#' powerful approach to multiple testing. \emph{J. R. Stat. Soc. B}, 57, 289-300.
-#' 
-#' Storey, J. D., Taylor, J. E. and Siegmund, D. (2004) Strong control, conservative point estimation and 
-#' simultaneous rates: a unified approach. \emph{J. R. Stat. Soc. B}, 66, 187- 205.
+#'             Storey, J. D., Taylor, J. E. and Siegmund, D. (2004)
+#'             Strong control, conservative point estimation and 
+#'             simultaneous rates: a unified approach. 
+#'             \emph{J. R. Stat. Soc. B}, 66, 187- 205.
 #' 
 #' @examples
-#' library(edgeR)
+#' library(limma)
 #' library(qvalue)
-#' arg = list(
-#'   nG = 10000,  
-#'   pi0 = 0.8,   
-#'   group = rep(c(1, 2), each = 13) 
-#' )
+#' m <- 14                      ## sample size per treatment group
+#' mu <- 10                     ## mean read counts in control group
+#' disp <- 0.1                  ## dispersion for all genes
+#' logfc <- log(2)              ## log fold change for DE genes
 #' 
-#' k <- 2                                   ## number of simulations (defined by user)
-#' fdr <- 0.05                              ## the false discovery rate to be controlled
-#' mu <- 10                                 ## mean counts in control group for all genes
-#' disp <- 0.1                              ## dispersion for all genes
-#' logfc <- log(2)                          ## log fold change for up-regulated genes
-#' 
-#' check.power(arg, k, fdr, mu, disp, logfc)
+#' check.power(m = m, mu = mu, disp = disp, logfc = logfc, sims = 2)
 #'
-#'  @export
+#' @export
 #' 
-check.power <- function(arg, k, fdr, mu, disp, logfc){
-  res <- list()
-  ## "power" & "fdr" function
+check.power <- function(nGenes = 10000, pi0 = 0.8, m, mu, disp, logfc, 
+                        up = 0.5, replace = TRUE, fdr = 0.05, sims = 100) {
+  
+  ## empirical "power" & "fdr" function
   powerfdr.fun <- function(fdr, p){
-    V <- sum((p < fdr) & (sim$de == FALSE))
-    R <- sum(p < fdr)
+    V <- sum( (p < fdr) & (sim$de == FALSE))
+    R <- sum( p < fdr )
     S <- R - V
-    power <- S / (arg$nG * (1 - arg$pi0))
+    power <- S / (nGenes * (1 - pi0))
     fdr_true <- V / R
     return(c(power, fdr_true))
   }
   
-  pow_bh <- fdr_bh <- pow_st <- fdr_st <- rep(0, k)
-  for(j in 1:k){
-    sim <- sim.counts(arg, mu, disp, logfc)
+  res <- list()
+  pow_bh <- fdr_bh <- pow_qvalue <- fdr_qvalue <- rep(0, sims)
+  for (j in 1:sims){
+    # message("Performing simulation ", j, "/", sims, "...")
+    sim <- sim.counts(nGenes, pi0, m, mu, disp, logfc, up, replace)
     cts <- sim$counts
     lib.size <- colSums(cts)
-    d <- DGEList(cts, lib.size, group = arg$group)
+    group = rep(c(1, 2), each = m)
+    d <- DGEList(cts, lib.size, group = group)
     d <- calcNormFactors(d)
-    design <- model.matrix(~ factor(arg$group))
-    y <- voom(d, design, plot=FALSE)       # convert read counts to log2-cpm with associated weights
+    design <- model.matrix(~ factor(group))
+    y <- voom(d, design, plot=FALSE)       # convert read counts to log2-cpm 
+                                           # with associated weights
     fit <- lmFit(y, design)
     fit <- eBayes(fit)
     pvalue <- fit$p.value[, 2]             # pvalue
     
-    p_bh <- p.adjust(pvalue, method="BH")  # Benjamini & Hochberg
+    p_bh <- p.adjust(pvalue, method = "BH")  # Benjamini & Hochberg
     pow_bh[j] <- powerfdr.fun(fdr, p_bh)[1]
     fdr_bh[j] <- powerfdr.fun(fdr, p_bh)[2]
   
-    p_st <- qvalue(pvalue)$qvalues         # Storey and Tibshirani
-    pow_st[j] <- powerfdr.fun(fdr, p_st)[1]
-    fdr_st[j] <- powerfdr.fun(fdr, p_st)[2]
+    p_qvalue <- qvalue(pvalue)$qvalues         # Storey and Tibshirani
+    pow_qvalue[j] <- powerfdr.fun(fdr, p_qvalue)[1]
+    fdr_qvalue[j] <- powerfdr.fun(fdr, p_qvalue)[2]
   }
+  # message("Simulations completed.")
     
-  ## average power & true fdr over k simulations
+  ## average power & true fdr over sims simulations
   res$pow_bh_ave <- mean(pow_bh)
   res$fdr_bh_ave <- mean(fdr_bh)
-  res$pow_st_ave <- mean(pow_st)
-  res$fdr_st_ave <- mean(fdr_st)
+  res$pow_qvalue_ave <- mean(pow_qvalue)
+  res$fdr_qvalue_ave <- mean(fdr_qvalue)
   return(res)
 }
--- R/sim.counts.R
+++ R/sim.counts.R
@@ -1,64 +1,94 @@
 #' RNA-seq Count Data Simulation from Negative-Binomial Distribution
 #' 
 #' This function simulates count data from Negative-Binomial distribution
-#' for RNA-seq experiments with given mean, dispersion and log fold change. 
+#' for two-sample RNA-seq experiments with given mean, dispersion 
+#' and log fold change. 
 #' A count data matrix is generated.
 #' 
 #' @import MASS
-#' @param arg a list of global parameters to pass into the function, such as total number of genes, 
-#' proportion of non-differentially expressed genes and treatment groups. See Details for more information.
-#' @param mu a vector (or scalar) of mean counts in control group from which to simulate.
-#' @param disp a vector (or scalar) of dispersion parameter from which to simulate.
-#' @param logfc a vector (or scalar) of log fold change between treatment group and control group.
-#' @param up proportion of up-regulated genes among all differentially expressed genes, the default value is \code{0.5}.
-#' @param replace sample with or without replacement from given parameters. See Details for more information.
+#' @param nGenes total number of genes, the default value is \code{10000}.
+#' @param pi0 proportion of non-differentially expressed genes, 
+#'            the default value is \code{0.8}.
+#' @param m sample size per treatment group.
+#' @param mu a vector (or scalar) of mean counts in control group 
+#'           from which to simulate.
+#' @param disp a vector (or scalar) of dispersion parameter 
+#'             from which to simulate.
+#' @param logfc a vector (or scalar, or a function that takes an integer n 
+#'                        and generates a vector of length n)
+#'              of log fold change for differentially expressed (DE) genes.  
+#' @param up proportion of up-regulated genes among all DE genes, 
+#'           the default value is \code{0.5}.
+#' @param replace sample with or without replacement from given parameters. 
+#'                See Details for more information.
 #' 
-#' @details \code{arg = list(nG, pi0, group)} where \code{nG} is the total number of genes, \code{pi0} 
-#' is the proportion of non-differentially expressed genes, and \code{group} is the treatment groups.
-#' 
-#' If the total number of genes is larger than length of \code{mu} or \code{disp}, 
-#' \code{replace} always equals \code{TRUE}.
+#' @details If the total number of genes \code{nGenes} is larger 
+#'          than length of \code{mu} or \code{disp}, 
+#'          \code{replace} always equals \code{TRUE}.
 #' 
 #' @return \item{counts}{RNA-seq count data matrix.}
 #' @return \item{group}{treatment group vector.}
 #' @return \item{lambda0}{mean counts in control group for each gene.}
 #' @return \item{phi0}{dispersion parameter for each gene.}
-#' @return \item{de}{differentially expressed genes indicator: \code{0} for non-differentially expressed genes, 
-#' \code{1} for up-regulated genes, \code{-1} for down-regulated genes.}
-#' @return \item{delta}{log fold change for each gene between treatment group and control group.}
+#' @return \item{de}{differentially expressed genes indicator: 
+#'                   \code{0} for non-differentially expressed genes, 
+#'                   \code{1} for up-regulated genes, 
+#'                   \code{-1} for down-regulated genes.}
+#' @return \item{delta}{log fold change for each gene between 
+#'                      treatment group and control group.}
 #' 
-#' @author Ran Bi \email{biran@@iastate.edu}, Peng Liu \email{pliu@@iastate.edu}
+#' @author Ran Bi \email{biran@@iastate.edu}, 
+#'         Peng Liu \email{pliu@@iastate.edu}
 #' 
 #' @examples
-#' arg <- list(nG = 10000,                      ## total number of genes
-#'             pi0 = 0.8,                       ## proportion of non-differentially expressed genes
-#'             group = rep(c(1, 2), each = 3))  ## treatment groups
-#' mu <- 10                                     ## mean counts in control group for all genes
-#' disp <- 0.1                                  ## dispersion for all genes
-#' logfc <- log(2)                              ## log fold change for up-regulated genes
+#' m <- 3                    ## sample size per treatment group
+#' mu <- 10                  ## mean counts in control group for all genes 
+#' disp <- 0.1               ## dispersion for all genes
+#' logfc <- log(2)           ## log fold change for DE genes
 #' 
-#' RNA_simu <- sim.counts(arg, mu, disp, logfc, up = 0.5, replace = TRUE)
-#' RNA_simu$counts                              ## count data matrix
+#' sim <- sim.counts(m = m, mu = mu, disp = disp, logfc = logfc)
+#' sim$counts                ## count data matrix
+#' 
+#' 
+#' ## varying fold change
+#' logfc1 <- function(x){rnorm(x, log(2), 0.5*log(2))}
+#' sim1 <- sim.counts(m = m, mu = mu, disp = disp, logfc = logfc1)
 #' 
 #' @export
 #' 
-sim.counts <- function(arg, mu, disp, logfc, up = 0.5, replace = TRUE){
+sim.counts <- function(nGenes = 10000, pi0 = 0.8, m, mu, disp, logfc, 
+                       up = 0.5, replace = TRUE){
+  arg <- list(nGenes = nGenes,
+              pi0 = pi0,
+              group = rep(c(1, 2), each = m))
+ 
+  ## expected false positives
+  FP <- round(nGenes * pi0)
+  TP <- nGenes - FP 
   
-  de = c(rep(0, arg$nG * arg$pi0), 
-         rep(1, (arg$nG - arg$nG * arg$pi0) * up),
-         rep(-1, (arg$nG - arg$nG * arg$pi0) * (1 - up)))
-  de = de[sample.int(length(de))] ## resample
+  ## types of true positives
+  TP_up <- round(TP * up)
+  TP_down <- TP - TP_up 
+
+  de <- c(rep(0, FP), rep(1, TP_up), rep(-1, TP_down))
+  de <- de[sample.int(length(de))] ## resample
   
-  # h = vector indicating which pseudo-genes to be re-simulated
-  h = rep(T, arg$nG) 
-  counts <- matrix(0, nrow = arg$nG, ncol = length(arg$group))
+  # h = vector indicating which pseudo-genes to re-simulate
+  h <- rep(TRUE, nGenes) 
+  counts <- matrix(0, nrow = nGenes, ncol = 2 * m)
   
-  delta <- logfc * de 
   ## log fold change, approximately half positive, half negative
+  delta <- rep(0, nGenes)
+  if (is.function(logfc)){
+    lfc <- logfc(TP)
+  }else{
+    lfc <- logfc
+  }
+  delta[de != 0] <- lfc * de[de != 0]
   
-  selected_genes <- true_means <- true_disps <- rep(0, arg$nG)
+  selected_genes <- true_means <- true_disps <- rep(0, nGenes)
   left_genes <- 1:length(mu)
-  lambda <- phi <- matrix(0, nrow = arg$nG, ncol = length(arg$group))
+  lambda <- phi <- matrix(0, nrow = nGenes, ncol = 2 * m)
   
   while(any(h)){
     temp <- sample.int(length(left_genes), sum(h), replace)
@@ -71,18 +101,21 @@ sim.counts <- function(arg, mu, disp, logfc, up = 0.5, replace = TRUE){
     true_means[h] <- mu[selected_genes[h]]
     true_disps[h] <- disp[selected_genes[h]]
     
-    lambda[h,] = matrix(true_means[h], ncol = 1) %*% matrix(rep(1, length(arg$group)), nrow = 1) * 
-      cbind(matrix(rep(1, sum(h) * length(arg$group)/2), ncol = length(arg$group)/2), matrix(rep(exp(delta[h]), length(arg$group)/2), ncol = length(arg$group)/2))
+    lambda[h,] <- matrix(true_means[h], ncol = 1) %*% 
+                  matrix(rep(1, 2 * m), nrow = 1) * 
+                  cbind(matrix(rep(1, sum(h) * m), ncol = m), 
+                        matrix(rep(exp(delta[h]), m), ncol = m))
     ## mean of counts
-    phi[h,] <- matrix(rep(true_disps[h], length(arg$group)), ncol = length(arg$group))
+    
+    phi[h,] <- matrix(rep(true_disps[h], 2 * m), ncol = 2 * m)
     ## dispersion of counts
     
-    counts[h,] = rnegbin(sum(h) * length(arg$group), lambda[h,], 1/phi[h,])
-    h = (rowSums(cpm(counts)>2) < 3)
+    counts[h,] <- rnegbin(sum(h) * 2 * m, lambda[h,], 1 / phi[h,])
+    h <- (rowSums(cpm(counts) > 2) < 3)
     # print(sum(h))
   }
   
-  if(any(rowSums(cpm(counts)>2) < 3 ))
+  if(any(rowSums(cpm(counts) > 2) < 3 ))
     print("Error: Failed to simulate data: some genes are not expressed.")
   
   list(counts = counts, 
